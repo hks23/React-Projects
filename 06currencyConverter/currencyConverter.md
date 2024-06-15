d15c8357f80cf9cab5b163950a987f90f9bdba54
Harsh Kr Singh <harshks.mit@gmail.com>
2024-06-16 02:37:21 +0530

    Currency Converter Project

---
+++ 06currencyConverter/currencyConverter.md
@@ -0,0 +1,5 @@
+let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+
+```JavaScript
+
+```
---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+//hook is nothing but just a function 
+import {useEffect, useState} from "react"
+
+function useCurrencyInfo(currency){
+    //useEffect hook takes a function {} and a dependency array []
+    const [data,setData] = useState({})
+    useEffect(()=> {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res)=> setData(res[currency]))
+
+        console.log(data);
+    }, [currency])
+
+    return data
+}
+
+export default useCurrencyInfo;
